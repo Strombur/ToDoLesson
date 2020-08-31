bf20c47ecc05db366274d3fec49c8a94a1ad3bfe
Necros <gamer7785666@gmail.com>
2020-08-31 14:30:05 +0300

    input

---
+++ script.js
@@ -0,0 +1,8 @@
+const selectAllButton = document.getElementById('selectAllButton');
+const inputElement = document.getElementById('input');
+
+
+
+selectAllButton.addEventListener('click', () => {
+    console.log('fired')
+})
